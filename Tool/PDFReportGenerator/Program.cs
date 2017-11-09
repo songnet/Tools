117c17c80a420e6db016e121ed03a7ebf1798058
宋鹏 <15712847805@163.com>
2017-11-09 18:41:39 +0800

    C# 压缩文件夹返回byte[]

--- Tool/PDFReportGenerator/Program.cs
+++ Tool/PDFReportGenerator/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CheckUtils;
+using static CheckUtils.FileCompress;
+using System.IO;
 
 namespace PDFReportGenerator
 {
@@ -10,18 +13,60 @@ namespace PDFReportGenerator
     {
         static void Main(string[] args)
         {
-            string sql = "1.1.1.";
+            string[] FileProperties = new string[2];
+            string htmlReportTemplatePath = "D:\\github\\Tools\\Tool\\PDFReportGenerator\\TemplateFiles";//待压缩文件目录
+            string zipPath = "D:\\TestFiles\\zip\\a.zip";  //压缩后的目标文件
 
-            char s = '.';
+            string temp = Directory.GetCurrentDirectory();
 
-            Array a = sql.Split(s);
+            string modelValue = "";
 
-            foreach (var i in a)
+            // 读取文件夹、文件，同时替换
+
+            List<ComplexFile> fileList = GetHtmlReportFiles(htmlReportTemplatePath, modelValue);
+
+            byte[] fileBytes = new FileCompress().Compress(fileList);
+            File.WriteAllBytes(zipPath, fileBytes);
+        }
+
+        private static List<ComplexFile> GetHtmlReportFiles(string htmlReportTemplatePath, string modelValue)
+        {
+            List<ComplexFile> complexFiles = new List<ComplexFile>();
+
+            string pathFormat = Path.Combine(htmlReportTemplatePath);
+            GetFiles(htmlReportTemplatePath, pathFormat, ref complexFiles);
+
+            return complexFiles;
+        }
+
+        private static void GetFiles(string path, string rootPath, ref List<ComplexFile> complexFiles)
+        {
+            string[] directories = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+
+            foreach (var directory in directories)
             {
-                Console.Write(i);
+                ComplexFile complexFile = new ComplexFile();
+                complexFile.Name = directory.Replace(rootPath, "");
+
+                // 文件夹
+                List<ComplexFile> complexFileChildren = complexFile.Children;
+                GetFiles(directory, rootPath, ref complexFileChildren);
+
+                complexFiles.Add(complexFile);
             }
 
-            Console.ReadLine();
+            foreach (var file in files)
+            {
+                ComplexFile complexFile = new ComplexFile();
+                complexFile.Name = file.Replace(rootPath, "");
+
+                // 文件
+                byte[] bytes = File.ReadAllBytes(file);
+                complexFile.fileBinary = bytes;
+
+                complexFiles.Add(complexFile);
+            }
         }
     }
 }
